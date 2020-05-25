0b94c8680935932afbe720ddc369cf234957a325
tungtran <tungtt309@gmail.com>
2020-05-25 17:54:19 +0700

    update menu bar

--- src/app/admin/child-routes.ts
+++ src/app/admin/child-routes.ts
@@ -3,51 +3,51 @@ export const childRoutes = [
     path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then(m => m.DashboardModule),
-    data: { icon: 'dashboard', text: 'Dashboard' }
+    data: { icon: 'dashboard', text: 'Home' }
   },
   {
-    path: 'charts',
+    path: 'statistic',
     loadChildren: () =>
       import('./charts/charts.module').then(m => m.ChartsModule),
-    data: { icon: 'bar_chart', text: 'Charts' }
+    data: { icon: 'bar_chart', text: 'Statistic' }
   },
   {
-    path: 'tables',
+    path: 'user',
     loadChildren: () =>
       import('./tables/tables.module').then(m => m.TablesModule),
-    data: { icon: 'table_chart', text: 'Tables' }
+    data: { icon: 'table_chart', text: 'User' }
   },
   {
-    path: 'forms',
+    path: 'drone',
     loadChildren: () => import('./forms/forms.module').then(m => m.FormsModule),
-    data: { icon: 'assignment', text: 'Forms' }
+    data: { icon: 'assignment', text: 'Drone' }
   },
   {
-    path: 'mat-grid',
+    path: 'history',
     loadChildren: () =>
       import('./mat-grid/mat-grid.module').then(m => m.MatGridModule),
-    data: { icon: 'grid_on', text: 'Flex Grid' }
-  },
-  {
-    path: 'mat-components',
-    loadChildren: () =>
-      import('./mat-components/mat-components.module').then(
-        m => m.MatComponentsModule
-      ),
-    data: { icon: 'code', text: 'Material Components' }
-  },
-  {
-    path: 'animations',
-    loadChildren: () =>
-      import('./animations/animations.module').then(m => m.AnimationsModule),
-    data: { icon: 'perm_media', text: 'Animations' }
-  },
-  {
-    path: 'google-maps',
-    loadChildren: () =>
-      import('./google-map-demo/google-map-demo.module').then(
-        m => m.GoogleMapDemoModule
-      ),
-    data: { icon: 'place', text: 'Google Maps' }
-  }
+    data: { icon: 'grid_on', text: 'History' }
+  }//,
+  // {
+  //   path: 'material',
+  //   loadChildren: () =>
+  //     import('./mat-components/mat-components.module').then(
+  //       m => m.MatComponentsModule
+  //     ),
+  //   data: { icon: 'code', text: 'Material' }
+  // },
+  // {
+  //   path: 'animations',
+  //   loadChildren: () =>
+  //     import('./animations/animations.module').then(m => m.AnimationsModule),
+  //   data: { icon: 'perm_media', text: 'Animations' }
+  // },
+  // {
+  //   path: 'google-maps',
+  //   loadChildren: () =>
+  //     import('./google-map-demo/google-map-demo.module').then(
+  //       m => m.GoogleMapDemoModule
+  //     ),
+  //   data: { icon: 'place', text: 'Google Maps' }
+  // }
 ];
